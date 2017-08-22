127c235af066bc6fde28c3499a30e75a27b394f2
Neville Oronni <nevooronni@gmail.com>
2017-08-22 15:37:51 +0300

    added array functionality and css

--- js/script.js
+++ js/script.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
     $("#result").show();
 
     $(".container").hide();
+    $("ul#array").slideToggle();
 
     $("#back").click(function() {
     	$("#result").hide();
@@ -24,6 +25,13 @@ $(document).ready(function() {
 		var birthday = $("input#date").val();
 		var favoriteTeam = $("select#selectboxes").val();
 		var girlType= $("input:radio[name=girl]:checked").val();
+		var array = [name, favoriteFood, favoriteMusic, favoriteColor, birthday, favoriteTeam, girlType];
+		var newArray = [];
+		var second = array[1];
+		var first = array[0];
+		var third = array[2];
+		newArray.push(second, first, third);
+
 
 		$("#input1").text(name);
 		$("#input2").text(favoriteFood);
@@ -32,7 +40,9 @@ $(document).ready(function() {
 		$("#input5").text(birthday);
 		$("#input6").text(favoriteTeam);
 		$("#input7").text(girlType);
-
+    $("#one").text(newArray[1]);
+    $("#two").text(newArray[0]);
+    $("#three").text(newArray[2]);
 
     
 	});
